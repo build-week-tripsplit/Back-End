c37cc88350762b4b3c69fc1397a8aa7aabc26efe
kmcknight1 <katiemcknight1991@gmail.com>
2019-08-26 16:46:20 -0500

    error message updated when registering a user

--- api/routers/authRouter.js
+++ api/routers/authRouter.js
@@ -24,7 +24,7 @@ router.post("/register", (req, res) => {
       res.status(201).json(saved);
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({ error, message: "Failed" });
     });
 });
 
