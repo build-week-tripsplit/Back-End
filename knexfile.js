234d17ad1370b3974cf9b5816caba3d3fe04d506
kmcknight1 <katiemcknight1991@gmail.com>
2019-08-26 15:45:02 -0500

    adds production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -26,5 +26,20 @@ module.exports = {
     seeds: {
       directory: "./database/seeds"
     }
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
   }
 };
