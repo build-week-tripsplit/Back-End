58c0581549b1d9289d30f9b247a9b988a8e58318
kmcknight1 <katiemcknight1991@gmail.com>
2019-08-28 11:51:53 -0500

    adds findUserExpenses function and endpoint

--- api/helpers/expenses-model.js
+++ api/helpers/expenses-model.js
@@ -21,7 +21,7 @@ function findBy(filter) {
 //ENDPOINT /api/expenses/user/:id
 
 async function findUserExpenses(user_id) {
-  const [expenses] = await db("expenses")
+  const expenses = await db("expenses")
     .where({ user_id })
     .returning("*");
 
