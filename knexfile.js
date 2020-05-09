8e905ec2ef7a1169866cde255b817784596b8987
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-09 04:33:07 -0500

    updated dependencies

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,15 @@ module.exports = {
   development: {
     client: "pg",
     useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    },
     connection: {
       database: process.env.DB_DEV_DATABASE,
       user: process.env.DB_DEV_USER,
